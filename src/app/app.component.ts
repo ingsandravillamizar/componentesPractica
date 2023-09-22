b792f3098072dbd7a97064494f4e333bd4b2288a
svillamizar <ingsandravillamizar@github.com>
2023-09-22 15:11:20 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'store';
+  title = 'Store Mil';
+  imgParent = ''
+ //"ahttps://www.w3schools.com/howto/img_avatar.png"
 }
