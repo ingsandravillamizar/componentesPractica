0c96bc0fc25517341b8c5ebebd3ee8a8ec876f5f
svillamizar <ingsandravillamizar@github.com>
2023-09-25 15:56:30 -0500

    ciclo de vida en componente imagen

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input , Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Input , Output, EventEmitter, OnChanges, AfterViewInit,OnDestroy } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
 
 @Component({
@@ -6,7 +6,8 @@ import { TitleStrategy } from '@angular/router';
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit{
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy  {
+
   @Input() img: string = 'valor inicial';
   @Input() titulo: string = 'My Store';
 
@@ -15,10 +16,39 @@ export class ImgComponent implements OnInit{
 
   imageDefault = 'https://forestal.cafe/img/cafe/coffee.png'
 
-  constructor() { }
+  constructor() {
+    //before render
+    // No correr aqui nada asincrono    No async
+    // esto corre una sola vez          One time
+    console.log('Constructor...', 'imgValue => ', this.img)
+  }
 
-  ngOnInit(): void{ }
 
+
+  ngOnChanges() {
+
+    //before y durante render   corre antes y durante de Renderizarse
+    //changes inputs    actuliza los cambios en los inputs
+    // corre muchas veces, tantas veces como actulicemos los inputs   times
+    console.log('ngOnchanges...', 'imgValue => ', this.img)
+   }
+
+   ngOnInit(): void {
+    //  before render     corre antes de Renderizarse
+    //  Aqui si podemos correr cosas async  -- fetch   ej llamar un api
+    //  Corre una sola vez
+    console.log('ngOnInit...', 'imgValue => ', this.img)
+   }
+
+   ngAfterViewInit() {
+    // Corre de despues   after render
+    //  handler children    :  Aqui se manejan los hijos.... manipularlos
+   }
+
+   ngOnDestroy() {
+    // delete -- once time   cuando eliminamos el componente,  no lo vemos en la interface
+    console.log('ngOnDestroy');
+  }
 
   imgError(){
       console.log("Hubo un error al carga imagen,  cargo la imagen interna por defecto")
