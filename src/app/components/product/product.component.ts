cb5b6dc734bc96d7a64de327dd3d79b4f5f1967d
svillamizar <ingsandravillamizar@github.com>
2023-09-25 15:36:33 -0500

    componente producto explicacion

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -22,3 +22,27 @@ export class ProductComponent implements OnInit {
 
   }
 }
+
+
+// ng  g c components/product
+// creamos una carpeta models al mismo nivel de components
+// creamos una interface del tipado de productos
+
+    // definimos un array tipo   Products: Product[] =[ donde el proposito es enviar desde el padre los productos
+    // por tanto en el html de component encontramos:
+    //<app-product [producto]="product" *ngFor="let product of Products "></app-product>
+    //  asi lo enviamos   [producto]="product"
+    //  asi enviamos cada uno de los elementos  *ngFor="let product of Products
+
+
+    // En el componente de productos tenemos que importar el modelo
+    // import { Product} from '../../models/product.model'  y el input  ...Input } from '@angular/core';
+
+       // @Input() producto: Product = {
+       //   id   : '',
+       //   name : '',
+       //   image : "",
+       //   price: 0
+       // }
+
+
